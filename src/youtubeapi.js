95960fd7bf491856900be9576622919e692e7efc
Tharun30 <abburitharun88@gmail.com>
2021-06-03 22:32:50 +0530

    fetching data from youtube api

--- src/youtubeapi.js
+++ src/youtubeapi.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './youtubeapi.css'
 import { useRef, useState } from "react";
+import Button from '@material-ui/core/Button';
 function Youtubeapi() {
     const [firstName, setFirstName] = useState('');
-   var text=' ';
+    const componentDidMount=()=> {
+        const apiUrl = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyA_-pgGuPTYypNGox45JMcC0u86u87Tu8I&q=play date&part=snippet,id&order=date&maxResults=20';
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => console.log('This is your data', data));
+      }
     return (
         <div className='youtube'>
             <input type='text' className='youtube_search' onChange={e => setFirstName(e.target.value)}></input>
-            
             <p className='text'>{firstName}</p>
+            <Button variant="contained" color="primary" onClick={componentDidMount} className='youtube_button'>Search</Button>
         </div>
     )
 }
