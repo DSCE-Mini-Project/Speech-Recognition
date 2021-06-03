f9f0b90d6088091aedc37c1b180ec6e2c4b11c5c
Tharun30 <abburitharun88@gmail.com>
2021-06-03 22:13:41 +0530

    youtube api

--- src/youtubeapi.js
+++ src/youtubeapi.js
@@ -2,12 +2,13 @@ import React from 'react'
 import './youtubeapi.css'
 import { useRef, useState } from "react";
 function Youtubeapi() {
+    const [firstName, setFirstName] = useState('');
    var text=' ';
     return (
         <div className='youtube'>
-            <input type='text' className='youtube_search' onChange={(e) =>{text=e.target.value;console.log(text); }}></input>
+            <input type='text' className='youtube_search' onChange={e => setFirstName(e.target.value)}></input>
             
-            <p className='text'>{text}</p>
+            <p className='text'>{firstName}</p>
         </div>
     )
 }
